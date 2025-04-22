c80d1aa9c5f7e643041d9a2c6840bc73140b137e
Victoire07 <victoireboutin07@gmail.com>
2025-04-22 04:17:38 +0200

    Amélioration version du shell simple (exécution sans arguments, sans PATH)

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -1,48 +1,58 @@
 #include "main.h"
 
 /**
- * main - Version petit shell qui imprimera qui exécute /bin/ls -l /tmp
+ * main - Version petit shell 0.1
  * Return: 0 si succès sinon retourne 1
  */
 
-int main(void)
-{
-	char *ligne_lue = NULL;
-	size_t taille_buffer = 0;
-	ssize_t nb_caracteres_lus;
-	pid_t pid_enfant;
-	int statut;
-	char *arguments[] = {"/bin/ls", "-l", "/tmp", NULL};
-
-	while (1)
-	{
-		printf("$ ");
-
-		nb_caracteres_lus = getline(&ligne_lue, &taille_buffer, stdin);
-
-		if (nb_caracteres_lus == -1)
-		{
-			free(ligne_lue);
-			exit(0);
-		}
-
-		pid_enfant = fork();
-
-		if (pid_enfant == -1)
-		{
-			perror("fork");
-			return (-1);
-		}
-		else if (pid_enfant == 0)
-		{
-			execve(arguments[0], arguments, environ);
-			perror("execve");
-			exit(1);
-		}
-		else
-		{
-			wait(&statut);
-		}
-
-	}
-}
+ int main(void)
+ {
+	 char *ligne_lue = NULL;
+	 size_t taille_buffer = 0;
+	 ssize_t nb_caracteres_lus;
+	 pid_t pid_enfant;
+	 int statut;
+	 char *arguments[2];
+ 
+	 while (1)
+	 {
+		 printf("$ ");
+		 nb_caracteres_lus = getline(&ligne_lue, &taille_buffer, stdin);
+ 
+		 if (nb_caracteres_lus == -1) /* Ctrl+D */
+		 {
+			 free(ligne_lue);
+			 exit(0);
+		 }
+ 
+		 /* Supprimer le \n à la fin de la ligne lue */
+		 if (ligne_lue[nb_caracteres_lus - 1] == '\n')
+			 ligne_lue[nb_caracteres_lus - 1] = '\0';
+ 
+		 pid_enfant = fork();
+		 if (pid_enfant == -1)
+		 {
+			 perror("fork");
+			 free(ligne_lue);
+			 exit(1);
+		 }
+		 else if (pid_enfant == 0)
+		 {
+			 arguments[0] = ligne_lue;
+			 arguments[1] = NULL;
+			 if (execve(arguments[0], arguments, environ) == -1)
+			 {
+				 perror("./hsh");
+				 exit(1);
+			 }
+		 }
+		 else
+		 {
+			 wait(&statut);
+		 }
+	 }
+ 
+	 free(ligne_lue);
+	 return (0);
+ }
+ 
