af89e9695db231ac733d107e93846c7efb955eb6
Victoire07 <victoireboutin07@gmail.com>
2025-04-22 23:12:26 +0200

    Création du fichier main.h et insertion du prototype pour main.c

--- main.h
+++ main.h
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 extern char **environ;
-void execute_command(char **argv, char *program_name);
-char **tokenize_line(char *line);
-char *_getenv(const char *nom_variable);
 
-#endif /* MAIN_H */
+void boucle_principale(void);
+
+#endif
