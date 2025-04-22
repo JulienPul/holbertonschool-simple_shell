e4521999d73044b8c73bda3d9b3125dcabb182a7
Victoire07 <victoireboutin07@gmail.com>
2025-04-22 02:28:52 +0200

    Ajout du premet jet du code shell 0.2

---
+++ simple_shell_0.2.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * main - ?? a faire après :)
+ */
+
+int main(void)
+{
+	char *ligne_lue = NULL;
+	size_t taille_buffer = 0;
+	ssize_t nb_caracteres_lus;
+	pid_t pid_enfant;
+	int statut;
+	char *arguments[] = {"/bin/ls", "-l", "/tmp", NULL};
+
+	while (1)
+	{
+		printf("$ ");
+
+		nb_caracteres_lus = getline(&ligne_lue, &taille_buffer, stdin);
+
+		if (nb_caracteres_lus == -1)
+		{
+			free(ligne_lue);
+			exit(0);
+		}
+
+		pid_enfant = fork();
+
+		if (pid_enfant == -1)
+		{
+			perror("fork");
+			return (-1);
+		}
+		else if (pid_enfant == 0)
+		{
+			execve(arguments[0], arguments, environ);
+			perror("execve");
+			exit(1);
+		}
+		else
+		{
+			wait(&statut);
+		}
+
+	}
+}
