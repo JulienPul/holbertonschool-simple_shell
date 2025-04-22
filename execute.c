0736086534d7fd2bc7d1fbe843f249d60f29eb3e
Victoire07 <victoireboutin07@gmail.com>
2025-04-23 01:07:29 +0200

    Ajout de la fonction execute_command pour exécuter une commande avec FWE

--- execute.c
+++ execute.c
@@ -7,5 +7,28 @@
 
 void execute_command(char *commande)
 {
-	
+	pid_t pid_enfant;
+	char *argv[] = {commande, NULL};
+
+	pid_enfant = fork();
+
+	if (pid_enfant == -1)
+	{
+		perror("fork");
+		return;
+	}
+
+	if (pid_enfant == 0)
+		
+	{
+		if (execve(commande, argv, environ) == -1)
+		{
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else 
+	{
+		wait(NULL);
+	}
 }
